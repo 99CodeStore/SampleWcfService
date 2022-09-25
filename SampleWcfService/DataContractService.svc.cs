c7526e35f55584c73c47d4a0c0c12d21041d058e
Bheemsen Singh <bheemstn@gmail.com>
2022-09-25 11:54:11 +0530

    DataContractAttribute and DataMemberAttribute

---
+++ SampleWcfService/DataContractService.svc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace SampleWcfService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "DataContractService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select DataContractService.svc or DataContractService.svc.cs at the Solution Explorer and start debugging.
+    public class DataContractService : IDataContractService
+    {
+        public Address GetAddress(int? Id)
+        {
+            return new Address
+            {
+                Addressline = "Sherganj",
+                City = "Sherganj",
+                PostalCode = "485001",
+                Country="India"
+            };
+        }
+    }
+}
---
+++ SampleWcfService/IDataContractService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace SampleWcfService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IDataContractService" in both code and config file together.
+    [ServiceContract]
+    public interface IDataContractService
+    {
+        [OperationContract]
+        Address GetAddress(int? Id);
+    }
+
+    [DataContract(Name = "Place", Namespace = "")]
+    public class Address
+    {
+
+        [DataMember(Name = "Addressline", Order = 1, IsRequired = true)]
+        public string Addressline;
+        [DataMember(Name = "City", Order = 2)]
+        public string City;
+        [DataMember(Name = "Country", Order = 3)]
+        public string Country;
+        [DataMember(Name = "PostalCode", Order = 4, IsRequired = true)]
+        public string PostalCode;
+    }
+
+}
